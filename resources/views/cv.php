f7f1ae09f6e5bf84b80143ab786233ab55be1ac0
Johnfx879 <ignchrisjohn31@gmail.com>
2023-09-13 19:45:01 +0700

    tugas profile pplg sitanggang 31

---
+++ resources/views/cv.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Curriculum Vitae</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+        h1 {
+            text-align: center;
+        }
+        h2 {
+            color: #333;
+        }
+        .header {
+            text-align: center;
+        }
+        .profile-image {
+          width: 150px;
+            height: 150px;
+            object-fit: cover;
+            border-radius: 50%;
+            margin: 10px auto;
+            display: block;
+        }
+        .section {
+            margin-bottom: 20px;
+        }
+        .section h2 {
+            border-bottom: 2px solid #333;
+        }
+        .section p {
+            margin: 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="header">
+        <h1>Curriculum Vitae</h1>
+        <img src="foto profil.jpeg" alt="Foto Profil" class="profile-image"> 
+        <p>Nama: Sitanggang Ignatius Christian John</p>
+        <p>Tempat & tanggal lahir: Bandung, 9 September 2006</p>
+        <p>Alamat: Jl. Asri No.49 RT.03 RW.06 Kab. Bandung</p>
+        <p>Email: ignchrisjohn31@gmail.com</p>
+        <p>Telepon: 082117812358</p>
+    </div>
+
+    <div class="section">
+        <h2>Riwayat Pendidikan</h2>
+        <p><strong>SDN 151 Suka Senang Bandung</strong></p>
+        <p>(2013-2019)</p>
+        <p><strong>SMP Santo Yusup Sulaksana</strong></p>
+        <p>(2019-2022)</p>
+        <p><strong>SMKN 2 Bandung</strong></p>
+        <p>(2022-2025)</p>
+    </div>
+
+    <div class="section">
+        <h2>Keterampilan</h2>
+        <p>- HTML, CSS</p>
+        <p>- Microsoft Office</p>
+        <p>- Kemampuan komunikasi yang baik</p>
+    </div>
+
+    <div class="section">
+        <h2>Hobi</h2>
+        <p>Menonton Film, bermain game, mendengarkan musik</p>
+    </div>
+</body>
+</html>
---
+++ resources/views/profilepplg.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Profile Company</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <section>
+        <header>
+            <a href="#"><img src="/image/logo.png" class="logo"></a>
+            <ul>
+                <a class="nav-link active" aria-current="page" href="#section1">BERANDA</a>
+                <li><a href="#">CV</a><li>
+
+                <li><a href="#">ABOUT</a><li>
+                <a class="nav-link" href="section4">CONTACT</a>
+            </ul>
+</header>
+<div class="content">
+    <div class="textbox">
+        <h2>Selamat Datang Di PPLG<br>Apa itu <span>PPLG?</span></h2>
+        <p>PPLG atau Pengembangan Perangkat Lunak dan Gim adalah sebuah jurusan di SMK (Sekolah Menengah Kejuruan). Jurusan ini mempelajari dan mendalami cara-cara mengembangkan perangkat lunak. Mulai dari pembuatan, pemeliharaan, manajemen kualitas, dan manajemen organisasi pengembangan perangkat lunak.
+           Selain itu, jurusan PPLG juga berhubungan dengan software komputer seperti pembuatan aplikasi, website, game dan semua yang berhubungan dengan pemrograman. Singkatnya, jurusan PPLG erat kaitannya dengan coding, desain, dan algoritma.</p>
+</div>
+<div class="imgBox">
+    <img src="/image/laptop.jpg" class="starbucks">
+</div>
+</div>
+</section>
+<div class="judul">
+<h1>Materi Jurusan PPLG</h1>
+<h3>Berikut ini merupakan Materi umum di Jurusan PPLG</h3>
+</div>
+<section class="konten" id="section2">
+    <div class="content-container">
+        <div class="box">
+            <div class="box-img">
+                <img src="/image/htmlicon.png" alt="">
+            </div>
+            <h2>HTML</h2>
+            <span>
+            HTML adalah bahasa yang digunakan untuk membuat halaman web. 
+            Bagi yang berkecimpung di dunia perangkat gadget dan ilmu komputer, 
+            pasti sudah tidak asing lagi dengan berbagai kode di dalamnya. 
+            HTML adalah kependekan dari Hypertext Markup Language.
+            </span>
+        </div>
+
+        <div class="box">
+            <div class="box-img">
+                <img src="/image/cssicon.png" alt="">
+            </div>
+            <h2>CSS</h2>
+            <span>
+            CSS adalah bahasa Cascading Style Sheet dan biasanya digunakan untuk mengatur 
+            tampilan elemen yang tertulis dalam bahasa markup, seperti HTML. 
+            CSS berfungsi untuk memisahkan konten dari tampilan visualnya di situs.
+            </span>
+        </div>
+
+        <div class="box">
+            <div class="box-img">
+                <img src="/image/phpicon.png" alt="">
+            </div>
+            <h2>PHP</h2>
+            <span>
+            Hypertext Preprocessor atau PHP adalah bahasa penulisan skrip open-source 
+            yang banyak digunakan dalam pemrograman atau pengembangan website (web development). 
+            Bahasa ini umumnya dijalankan dalam komunikasi sisi server, dan saat ini didukung oleh hampir semua sistem.
+            </span>
+        </div>
+    </div>
+    </section>
+    <section class="scroll-section" id="section3">
+        <h1 class='judul2'>Tentang Kami</h1>
+        <p class='judull2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Dignissimos odit ab earum. Rem harum, earum facilis modi commodi fugit 
+            totam optio ipsa quisquam voluptatum voluptas. Harum veniam eligendi eveniet ipsam!</p>
+        <div class="last">
+        <i class="fa-brands fa-facebook fa-xl" style="color: #000000;"></i>
+        <i class="fa-brands fa-square-instagram fa-xl" style="color: #000000;"></i>
+        <i class="fa-brands fa-telegram fa-xl" style="color: #000000;"></i>
+        <i class="fa-brands fa-github fa-xl" style="color: #000000;"></i>
+            </div>  
+    </section>
+    <div class="footer">
+    <p>&copy; 2023 PPLG 2. All rights reserved.</p>
+    <p>Alamat: Jl. Ciliwung No. 4, Kota Bandung, Provinsi Jawa Barat</p>
+    <p>Telepon: (022) 12345678 | Email: <a href="mailto:info@perusahaan.com">info@perusahaan.com</a> | Media Sosial: <a href="https://www.facebook.com/perusahaan">Facebook</a>, <a href="https://www.twitter.com/perusahaan">Twitter</a>, <a href="https://www.instagram.com/perusahaan">Instagram</a></p>
+  </div>
+</body>
+</html>
